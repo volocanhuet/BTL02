4974abc325b269fe895f9106cc828df6bb960f48
volocanhuet <43331022+volocanhuet@users.noreply.github.com>
2018-11-22 22:36:41 +0700

    Add files via upload

---
+++ src/com/bom/bomberman/entities/tile/destroyable/BrickTile.java
@@ -0,0 +1,49 @@
+package com.bom.bomberman.entities.tile.destroyable;
+
+
+import com.bom.bomberman.entities.Entity;
+import com.bom.bomberman.entities.bomb.DirectionalExplosion;
+import com.bom.bomberman.entities.mob.enemy.Kondoria;
+import com.bom.bomberman.graphics.Screen;
+import com.bom.bomberman.graphics.Sprite;
+import com.bom.bomberman.level.Coordinates;
+
+public class BrickTile extends DestroyableTile {
+	
+	public BrickTile(int x, int y, Sprite sprite) {
+		super(x, y, sprite);
+	}
+	
+	@Override
+	public void update() {
+		super.update();
+	}
+	
+	@Override
+	public void render(Screen screen) {
+		int x = Coordinates.tileToPixel(_x);
+		int y = Coordinates.tileToPixel(_y);
+		
+		if(_destroyed) {
+			_sprite = movingSprite(Sprite.brick_exploded, Sprite.brick_exploded1, Sprite.brick_exploded2);
+			
+			screen.renderEntityWithBelowSprite(x, y, this, _belowSprite);
+		}
+		else
+			screen.renderEntity( x, y, this);
+	}
+	
+	@Override
+	public boolean collide(Entity e) {
+		
+		if(e instanceof DirectionalExplosion)
+			destroy();
+		
+		if(e instanceof Kondoria)
+			return true;
+			
+		return false;
+	}
+	
+	
+}
