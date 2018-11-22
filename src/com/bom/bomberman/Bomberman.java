64250e94f0841ff3c0c85dd20077b01e227d4876
volocanhuet <43331022+volocanhuet@users.noreply.github.com>
2018-11-22 22:19:04 +0700

    Add files via upload

---
+++ src/com/bom/bomberman/Bomberman.java
@@ -0,0 +1,11 @@
+package com.bom.bomberman;
+
+import com.bom.bomberman.exceptions.BombermanException;
+import com.bom.bomberman.gui.Frame;
+
+public class Bomberman {
+	
+	public static void main(String[] args) throws BombermanException {
+		Frame mainwindow = new Frame();
+	}
+}
